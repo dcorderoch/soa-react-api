3d02363ee1e62615764bee4116a156308a5f77e1
David Cordero Chavarría <dcorderoch@ieee.org>
2021-08-07 15:07:12 -0600

    refactor js with prettier

--- TallerSOA/app.js
+++ TallerSOA/app.js
@@ -1,49 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const cors = require('cors');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', (req, res, next) => {
+app.use("/", (req, res, next) => {
   // NOTE: check if content type is in headers, and if it is application/json
-  let contype = req.headers['content-type'];
-  if(!contype || contype.indexOf('application/json') !== 0)
-  {
+  let contype = req.headers["content-type"];
+  if (!contype || contype.indexOf("application/json") !== 0) {
     // NOTE: if if content-type is null/undefined, or is NOT appication/json
     // return an error
     // TODO: check HTTP error codes to see if 405 is appropriate
-    req.status(405).send({error: true, message: "unsuppoerted MIME type"});
+    req.status(405).send({ error: true, message: "unsuppoerted MIME type" });
   }
   indexRouter(req, res, next);
 });
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.status(405).send({error:true, message:"método no existente"})
+  res.status(405).send({ error: true, message: "método no existente" });
 });
 
 let port = 3088; // NOTE: add variable to make this searchable
-app.listen(port)
+app.listen(port);
 
 module.exports = app;
--- test_rest.sh
+++ test_rest.sh
@@ -9,58 +9,72 @@ main() {
   local headers
   headers="Content-type: application/json"
 
+  echo "expect []"
   curl -H "$headers" -X GET    "$HOST/spaces"
   echo ""
   echo ""
 
+  echo "expect error:false"
   curl -H "$headers" -X POST   "$HOST/spaces"
   echo ""
   echo ""
 
+  echo "expect id:0"
   curl -H "$headers" -X GET    "$HOST/spaces"
   echo ""
   echo ""
 
+  echo "expect id:0"
   curl -H "$headers" -X GET    "$HOST/spaces/0"
   echo ""
   echo ""
 
+  echo "expect error:true"
   curl -H "$headers" -X GET    "$HOST/spaces/1"
   echo ""
   echo ""
 
+  echo "expect error:false"
   curl -H "$headers" -X PUT    "$HOST/spaces/0" -d '{"state":"in-use"}'
   echo ""
   echo ""
 
+  echo "expect id:0"
   curl -H "$headers" -X GET    "$HOST/spaces/0"
   echo ""
   echo ""
 
+  echo "expect reservations === []"
   curl -H "$headers" -X GET    "$HOST/reservations"
   echo ""
   echo ""
 
+  echo "expect error:false"
   curl -H "$headers" -X POST   "$HOST/reservations"
   echo ""
   echo ""
 
+  echo "expect reservations === [{...}]"
   curl -H "$headers" -X GET    "$HOST/reservations"
   echo ""
   echo ""
 
+  echo "expect error:false"
   curl -H "$headers" -X DELETE "$HOST/reservations/0"
   echo ""
   echo ""
 
+  echo "expect reservations === []"
   curl -H "$headers" -X GET    "$HOST/reservations"
   echo ""
   echo ""
 
+  echo "expect error:false"
   curl -H "$headers" -X DELETE "$HOST/spaces/0"
   echo ""
   echo ""
 
+  echo "expect []"
   curl -H "$headers" -X GET    "$HOST/spaces"
   echo ""
   echo ""

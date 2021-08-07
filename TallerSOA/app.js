5367bb6563ee21811b1c7d6a8ef684ae3f4f9130
Dario Rodriguez <darior1227@gmail.com>
2021-08-07 12:53:03 -0600

    API tested

--- TallerSOA/app.js
+++ TallerSOA/app.js
@@ -3,16 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -38,6 +37,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3005)
+app.listen(3088)
 
 module.exports = app;
--- TallerSOA/routes/index.js
+++ TallerSOA/routes/index.js
@@ -1,16 +1,117 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var spaces=[];
+var counter=0;
 
 
 
-router.get('/test',function(req,res,next){
-  console.log('Test1')
-  res.status(200).send({error:false})
+router.get('/spaces',function(req,res,next){
+  if(req.body.state!=undefined){
+    let result = []
+    for(let i = 0; i<spaces.length;i++){
+      if(spaces[i].state==req.body.state){
+        result.push(spaces[i])
+      }
+    }
+    res.status(200).send(result);
+  }
+  else{
+    res.status(200).send(spaces);
+
+  }
 })
+
+
+router.get('/spaces/:id',function(req,res,next){
+  let result;
+  for(let i = 0; i<spaces.length;i++){
+    if(spaces[i].id==Number(req.params.id)){
+      result=spaces[i]
+      console.log(result)
+      res.status(200).send(result);
+      return
+    }
+  }
+  res.send({error:true,message:"No se existe espacio con el id indicado"})
+
+})
+
+router.post('/spaces',function(req,res,next){
+  let space=`{"id":${counter},"state":"free","data":"original","carID":"","date":""}`
+  console.log(JSON.parse(space))
+  spaces.push(JSON.parse(space));
+  counter++;
+
+  res.status(200).send({error:false,message:"éxito al crear espacio"});
+})
+
+router.put('/spaces/:id',function(req,res,next){
+  let result;
+  for(let i = 0; i<spaces.length;i++){
+    if(spaces[i].id==req.params.id){
+      result=spaces[i]
+      result.data="modified"
+      res.send({error:false,message:"éxito al actulizar"})
+      return;
+    }
+  }
+  res.send({error:true,message:"No existe el espacio"})
+})
+router.delete('/spaces/:id',function(req,res,next){
+  for(let i = 0; i<spaces.length;i++){
+    if(spaces[i].id==req.params.id){
+      if(spaces[i].state=="free"){
+        spaces.splice(i,1)
+        res.send({error:false,message:"El espacio fué eliminado"})
+      }
+      else{
+        res.send({error:true,message:"El espacio está ocupado"})
+      }
+      return;
+    }
+  }
+  res.send({error:true,message:"No existe el espacio"})
+})
+
+
+
+router.get('/reservations',function(req,res,next){
+  result=[]
+  for(let i = 0; i<spaces.length;i++){
+    if(spaces[i].state=="in-use"){
+      result.push(spaces[i])
+    }
+  }
+  res.status(200).send(result);
+})
+
+router.post('/reservations',function(req,res,next){
+  for(let i = 0; i<spaces.length;i++){
+    if(spaces[i].state=="free"){
+      spaces[i].state="in-use"
+      spaces[i].carID=req.body.carID
+      spaces[i].date= new Date()
+      res.send({error:false,message:"éxito al reservar"})
+      return;
+    }
+
+  }
+  res.send({error:true,message:"No hay espacios disponibles"})
+})
+
+router.delete('/reservations/:id',function(req,res,next){
+  for(let i = 0; i<spaces.length;i++){
+    if(spaces[i].id==req.params.id){
+      spaces[i].state='free'
+      spaces[i].carID==''
+      spaces[i].date==''
+      res.send({error:false,message:"Reservación eliminada"})
+      return;
+    }
+  }
+  res.send({error:true,message:"no existe la reservación"})
+})
+
 
 module.exports = router;

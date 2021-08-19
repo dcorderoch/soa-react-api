89e53e627e8caab04ebc73a8712167e6685ddb04
David Cordero Chavarría <dcorderoch@ieee.org>
2021-08-18 18:17:21 -0600

    Add swagger and utility scripts

---
+++ run_http.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# shellcheck -x -s sh
+#             |
+#             `-> important
+
+main() {
+
+
+  # shellcheck source=rest/venv/bin/activate
+  if [ -e rest/venv/bin/activate ]; then
+    . rest/venv/bin/activate
+  fi
+
+  cd rest || return
+  flask run
+
+}
+
+main
